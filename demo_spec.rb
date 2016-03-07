8a5cac29ab15cf1a94a4d3c0eb928cbbbdf1aec5
Ivan <githubh@gmail.com>
2016-03-07 21:56:12 +0200

    Add test for power_up

--- demo_spec.rb
+++ demo_spec.rb
@@ -1,9 +1,17 @@
 require './demo'
 
 describe Demo do 
+ 
   it "has a capitalized name" do
     demo = Demo.new 'lorem ipsum'
 
     expect(demo.name).to eq 'Lorem ipsum'
   end
+
+  it "can power up" do
+    demo = Demo.new 'lorem ipsum'
+
+    expect(demo.power_up).to eq 110
+  end
+
 end
