42ec2bced06ba350889add1d6772c844b7d86c27
Ivan <githubh@gmail.com>
2016-03-13 22:18:32 +0200

    refactoring demo_spec.rb

--- demo_spec.rb
+++ demo_spec.rb
@@ -1,29 +1,25 @@
 require './demo'
 
 describe Demo do 
+
+  before do
+    @demo = Demo.new 'lorem ipsum'
+  end
  
   it "has a capitalized name" do
-    demo = Demo.new 'lorem ipsum'
-
-    expect(demo.name).to eq 'Lorem ipsum'
+      expect(@demo.name).to eq 'Lorem ipsum'
   end
 
   it "can power up" do
-    demo = Demo.new 'lorem ipsum'
-
-    expect(demo.power_up).to eq 110
+      expect(@demo.power_up).to eq 110
   end
 
   it"can power down" do
-    demo = Demo.new 'lorem ipsum'
-
-    expect(demo.power_down).to eq 90
+    expect(@demo.power_down).to eq 90
   end
 
   it"displays full hero info" do 
-    demo = Demo.new'lorem ipsum'
-
-    expect(demo.demo_info).to eq 'Lorem ipsum has a health of 100'
+      expect(@demo.demo_info).to eq 'Lorem ipsum has a health of 100'
   end 
 
 end
